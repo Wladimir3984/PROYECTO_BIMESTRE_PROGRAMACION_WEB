be998d7d00a2fae6851b4a0547b24b227e96a89a
Wladimir <wl.urzua@duocuc.cl>
2023-03-30 22:21:59 -0300

    proyecto Django creado, ya migre completamente en index y cree un base.html para ahí poner lo que se deberia reciclar en todas las paginas(navbar, footer, cargar bootstrap,js etc)

---
+++ gamesite/app/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def index(request):
+    return render(request, 'app/index.html')
+def aventura(request):
+    return render(request, 'app/aventura.html')
+def plataforma(request):
+    return render(request, 'app/plataforma.html')
+def guerra(request):
+    return render(request, 'app/guerra.html')
+def terror(request):
+    return render(request, 'app/terror.html')
+def rpg(request):
+    return render(request, 'app/rpg.html')
+def loggin(request):
+    return render(request, 'app/loggin.html')
+def formulario(request):
+    return render(request, 'app/formulario.html')
+
