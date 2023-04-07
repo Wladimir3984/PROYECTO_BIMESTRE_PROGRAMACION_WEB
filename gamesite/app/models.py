745321d1c76e6744c97e5d16c91ee453ba06eec3
wladimir3984 <wiuc2000@hotmail.cl>
2023-04-07 13:43:54 -0400

    oracle database integrated

--- gamesite/app/models.py
+++ gamesite/app/models.py
@@ -1,3 +1,9 @@
 from django.db import models
-
 # Create your models here.
+
+class Categoria(models.Model):
+    id_categoria = models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=25)
+
+    def __str__(self):
+        return self.nombre
--- gamesite/gamesite/settings.py
+++ gamesite/gamesite/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'gamesite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.oracle',
+        'NAME': 'xe',
+        'USER': 'USR_GAMESITE',
+        'PASSWORD': 'duoc123456',
+        'HOST': '',
+        'PORT': '',
     }
 }
 
