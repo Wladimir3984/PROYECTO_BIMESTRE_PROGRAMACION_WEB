ed2fef924b22c91a3b03550c7aa23718a74672f6
IgneL462 <d.barrigamolina@gmail.com>
2023-04-10 14:32:00 -0400

    update

--- gamesite/auth_admin/views.py
+++ gamesite/auth_admin/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
-from .forms import InsertGameForm
-from app.models import Juego
+from django.shortcuts import render, redirect, get_object_or_404
+from .forms import InsertGameForm, InsertUserForm
+from app.models import Juego, Usuario
 # Create your views here.
+
 
 def agregar_juego(request):
     data = {
@@ -18,9 +19,36 @@ def agregar_juego(request):
 
     return render(request, 'auth_admin/juegos_crud/agregar.html', data)
 
+
 def listar_juegos(request):
     juegos = Juego.objects.all()
     data = {
-        'juegos':juegos
+        'juegos': juegos
     }
-    return render(request, 'auth_admin/juegos_crud/listar.html',data)
+    return render(request, 'auth_admin/juegos_crud/listar.html', data)
+
+
+def modificar_usuario(request, id):
+    usuario = get_object_or_404(Usuario, id_usuario=id)
+
+    data = {
+        'form': InsertUserForm(instance=usuario)
+    }
+    if request.method == 'POST':
+        insert_form = InsertUserForm(
+            data=request.POST, instance=usuario, files=request.FILES)
+        if insert_form.is_valid():
+            insert_form.save()
+            data["mensaje"] = "modificado correctamente"
+            return redirect(to="listar_usuarios")
+        else:
+            data['mensaje'] = "Error al agregar el juego"
+            data['insert_form'] = insert_form
+    return render(request, 'auth_admin/usuario_crud/modificar.html', data)
+
+def listar_usuarios(request):
+    usuarios = Usuario.objects.all()
+    data = {
+        'juegos': usuarios
+    }
+    return render(request, 'auth_admin/usuario_crud/listar.html', data)
