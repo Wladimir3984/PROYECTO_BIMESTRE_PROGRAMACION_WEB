db1223c1ee96b09e451be4f2fd0eda3eff97d086
wladimir3984 <wiuc2000@hotmail.cl>
2023-04-12 13:37:59 -0400

    view of admin require permissions

--- gamesite/app/forms.py
+++ gamesite/app/forms.py
@@ -3,14 +3,6 @@ from django.contrib.auth.models import User
 from django import forms
 
 class CustomUserCreationForm(UserCreationForm):
-    # class Meta:
-    #     model = User
-    #     fields = ('username', 'email', 'password1', 'password2')
-
-    # def save(self, commit=True):
-        # user = super(CustomUserCreationForm, self).save(commit=False)
-        # user.email = self.cleaned_data["email"]
-        # if commit:
-        #     user.save()
-        # return user
-        pass
+    class Meta:
+        model = User
+        fields = ("username", "email", "password1", "password2")
--- gamesite/auth_admin/views.py
+++ gamesite/auth_admin/views.py
@@ -4,23 +4,33 @@ from app.models import Juego
 # Create your views here.
 
 def agregar_juego(request):
-    data = {
-        'insert_form': InsertGameForm()
-    }
-    if request.method == 'POST':
-        insert_form = InsertGameForm(data=request.POST)
-        if insert_form.is_valid():
-            insert_form.save()
-            data['mensaje'] = "Juego agregado correctamente"
-        else:
-            data['mensaje'] = "Error al agregar el juego"
-            data['insert_form'] = insert_form
+    is_supervisor = request.user.groups.filter(name='supervisor').exists()
+    is_superuser = request.user.is_superuser
+    if not is_supervisor and not is_superuser:
+        return render(request, 'app/index.html')
+    else:
+        data = {
+            'insert_form': InsertGameForm()
+        }
+        if request.method == 'POST':
+            insert_form = InsertGameForm(data=request.POST)
+            if insert_form.is_valid():
+                insert_form.save()
+                data['mensaje'] = "Juego agregado correctamente"
+            else:
+                data['mensaje'] = "Error al agregar el juego"
+                data['insert_form'] = insert_form
 
-    return render(request, 'auth_admin/juegos_crud/agregar.html', data)
+        return render(request, 'auth_admin/juegos_crud/agregar.html', data)
 
 def listar_juegos(request):
-    juegos = Juego.objects.all()
-    data = {
-        'juegos':juegos
-    }
-    return render(request, 'auth_admin/juegos_crud/listar.html',data)
+    is_supervisor = request.user.groups.filter(name='supervisor').exists()
+    is_superuser = request.user.is_superuser
+    if not is_supervisor and not is_superuser:
+        return render(request, 'app/index.html')
+    else:
+        juegos = Juego.objects.all()
+        data = {
+            'juegos':juegos
+        }
+        return render(request, 'auth_admin/juegos_crud/listar.html',data)
