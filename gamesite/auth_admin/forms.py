0f9acfbb81a8dbdde0ef4b75f7356f258060a688
wladimir3984 <wiuc2000@hotmail.cl>
2023-04-14 23:48:09 -0400

    cambio fuente. No pude cambiar BG

--- gamesite/auth_admin/forms.py
+++ gamesite/auth_admin/forms.py
@@ -12,7 +12,7 @@ from app.models import Juego, Usuario
 class InsertGameForm(forms.ModelForm):
     class Meta:
         model = Juego
-        fields = ['nombre', 'precio_venta', 'stock', 'id_categoria', 'descripcion', 'disponible']
+        fields = ['nombre', 'precio', 'stock', 'id_categoria', 'descripcion']
 
 
 class InsertUserForm(forms.ModelForm):
