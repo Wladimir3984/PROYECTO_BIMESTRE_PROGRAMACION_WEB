d38b47bae58d352444167a49ae45ba0b01b4466e
wladimir3984 <wiuc2000@hotmail.cl>
2023-04-17 17:43:52 -0400

    secret key protected

--- gamesite/gamesite/settings.py
+++ gamesite/gamesite/settings.py
@@ -20,7 +20,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-)%hliycfz5^1hg@(mk8v#b5jrkd3m5(z(+b940#*_czx=6$a2h'
+from decouple import config
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
