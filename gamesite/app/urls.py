b992d7429c7b0d349dceed2c047575b5b5805507
wladimir3984 <wiuc2000@hotmail.cl>
2023-04-20 14:29:45 -0400

    categorias y juegos dinamicos BD

--- gamesite/app/urls.py
+++ gamesite/app/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
-from .views import index, aventura, plataforma, guerra, terror, rpg, registro
+from .views import index, registro, categoria
 from django.contrib.auth import views as auth_views
 from .views import MyPasswordChangeView, CategoriaViewSet
 from rest_framework import routers
@@ -12,11 +12,7 @@ router.register('categoria', CategoriaViewSet)
 urlpatterns = [
     path('', index, name='index'),
     #catalogo de videojuegos
-    path('catalogo/aventura/', aventura, name='aventura'),
-    path('catalogo/plataforma/', plataforma, name='plataforma'),
-    path('catalogo/guerra/', guerra, name='guerra'),
-    path('catalogo/terror/', terror, name='terror'),
-    path('catalogo/rpg/', rpg, name='rpg'),
+    path('catalogo/<str:nombre>/', categoria, name='categoria'),
     #registro
     path('registro/', registro, name='registro'),
     #cambiar contraseña
