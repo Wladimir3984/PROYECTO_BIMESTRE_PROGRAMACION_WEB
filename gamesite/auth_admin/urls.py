fabc92d4d26df311a6206f37b90798f149bfe530
IgneL462 <d.barrigamolina@gmail.com>
2023-05-05 19:15:25 -0400

    se agrego casilla para ingresar id de juego a eliminar

--- gamesite/auth_admin/urls.py
+++ gamesite/auth_admin/urls.py
@@ -7,7 +7,7 @@ from .views import agregar_juego, listar_juegos, modificar_usuario, listar_usuar
 urlpatterns = [
     path('usroetnu2342342348rnteoenturoeuo213/',
          agregar_juego, name='agregar_juego'),
-    path('usroetnu2342342348rnteoenturoeuo213/<id>/',
+    path('',
          eliminarJuego, name='eliminarJuego'),
     path('usroetnu2342342348rnteoenturoeuo214/',
          listar_juegos, name='listar_juegos'),
